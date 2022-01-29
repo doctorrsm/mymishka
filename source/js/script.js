81f79d6e9996ead279625b7dd0ec539f7d69d2eb
Sergey Rutkovsky <dr.bibelman@gmail.com>
2022-01-29 11:02:33 +0300

    Финал верстки

--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,16 @@
-'use strict';
 // const noJsMap = document.querySelector('.contacts__map');
 //noJsMap.classList.remove('contacts__map--no-js');
-const noJsClass = document.querySelector('.navbar');
-noJsClass.classList.remove('navbar--no-js');
+const noJsClass = document.querySelector('.navbar__container');
+noJsClass.classList.remove('navbar__container--no-js');
 const navbarContainer = document.querySelector('.navbar__container');
 const mainNav = document.querySelector('.main-nav');
 const toogleButton = document.querySelector('.navbar__close-button');
 const linkswrapper = document.querySelector('.navbar__links-wrapper');
 toogleButton.onclick = function() {
-  navbarContainer.classList.toogle('navabar--visible');
-  mainNav.classList.toggle('main-nav--visible');
+  navbarContainer.classList.toggle('navbar__container--visible');
+  //mainNav.classList.toggle('main-nav--visible');
   toogleButton.classList.toggle('navbar__close-button--active');
-  linkswrapper.classList.toggle('navbar__links-wrapper--visible');
+  //linkswrapper.classList.toggle('navbar__links-wrapper--visible');
 };
 
 /* slider*/
